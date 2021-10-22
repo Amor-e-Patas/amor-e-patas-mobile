592f348097d7d4ab560386458bd4d427cc8a14cd
Uelyda Luiza <uelyda-luiza@hotmail.com>
2021-10-22 18:08:45 -0300

    Código não usado apagado

--- src/service/services.ts
+++ src/service/services.ts
@@ -1,10 +1,6 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-interface Headers {
-  Authorization: string;
-}
-
 const api = axios.create({
   baseURL: "http://192.168.1.69:3333/",
 });
@@ -21,7 +17,7 @@ export const authenticatedAPI = axios.create(
 
 export async function createAuthenticatedAPI() {
   //na hora de fazer login usa isso
-  await AsyncStorage.setItem("@amor-e-patas:user-token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF91c3VhcmlvIjoxLCJyb2xlIjoidXNlciIsImlkX2xvZ2luIjoxLCJpZF9lbmRlcmVjbyI6MSwiaWRfdGVsZWZvbmUiOjEsImlhdCI6MTYzNDg2ODI5NH0.P33BVclWwMDeEgPp9_3zjgFxDQmIrcwXa3FUuwGWAYQ");
+  //await AsyncStorage.setItem("@amor-e-patas:user-token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF91c3VhcmlvIjoxLCJyb2xlIjoidXNlciIsImlkX2xvZ2luIjoxLCJpZF9lbmRlcmVjbyI6MSwiaWRfdGVsZWZvbmUiOjEsImlhdCI6MTYzNDg2ODI5NH0.P33BVclWwMDeEgPp9_3zjgFxDQmIrcwXa3FUuwGWAYQ");
   
   const token = await AsyncStorage.getItem("@amor-e-patas:user-token");
   console.log(token, "token - services");
