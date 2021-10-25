35cebe85fdafb866da11b26bbbfd64e753ad3e3c
ana.santana <anaflavia2309@gmail.com>
2021-10-25 20:11:51 -0300

    Adicionando animal as rotas

--- src/routes/AuthRoutes.routes.tsx
+++ src/routes/AuthRoutes.routes.tsx
@@ -4,10 +4,12 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import Login from '../screens/Login';
 import SignUp from '../screens/SignUp';
 import Home from "../screens/Home/home";
+import Animal from "../screens/Animal/criaranimal"
 export type AuthRoutesParamList = {
   'Cadastre-se': undefined
   'Login': undefined
   'Home': undefined
+  'Cadastro de Animal': undefined
 }
 
 
@@ -29,6 +31,7 @@ function AuthRoutes() {
       <AuthStack.Screen name="Home" component={Home} />
       <AuthStack.Screen name="Login" component={Login} />
       <AuthStack.Screen name="Cadastre-se" component={SignUp} />
+      <AuthStack.Screen name="Cadastro de Animal" component={Animal} />
     </AuthStack.Navigator>
   );
 }
@@ -40,6 +43,7 @@ export function MyDrawer() {
       <Drawer.Screen name="Home" component={Home}  />
       <Drawer.Screen name="Login" component={Login} />
       <Drawer.Screen name="Cadastre-se" component={SignUp}/>
+      <AuthStack.Screen name="Cadastro de Animal" component={Animal} />
     </Drawer.Navigator>
   
   );
