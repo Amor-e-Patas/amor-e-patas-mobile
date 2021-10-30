86e1adbf9b6dfb5941a34f176fc5c58a47eda1ed
Uelyda Luiza <uelyda-luiza@hotmail.com>
2021-10-29 21:39:32 -0300

    Upload de imagens de animais

--- src/screens/Animal/criaranimal.tsx
+++ src/screens/Animal/criaranimal.tsx
@@ -17,7 +17,7 @@ import { AuthRoutesParamList } from "../../routes/AuthRoutes.routes";
 import axios from "axios";
 import { getAnimaisDesaparecidos } from "../../service/animal";
 import { criarAnimal } from "../../service/animal";
-import {criarImgAnimal} from "../../service/img_animal";
+import { criarImgAnimal } from "../../service/img_animal";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { fontFamily } from "../../constants/theme";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
@@ -40,8 +40,9 @@ interface Vive {
   descricao: string;
 }
 
-interface Imagem{
+interface Imagem {
   uri: string,
+  height: number,
   type: string
 }
 
@@ -63,67 +64,48 @@ function Animal() {
   const [selectSoci, setSelectSoci] = useState(Array<Number>());
   const [vivencias, setVivencia] = useState(Array<Vive>());
   const [selectVive, setSelectVive] = useState(Array<Number>());
-  //const [images, setImages] = useState<File[]>([]);
   const [images, setImages] = useState<Imagem[]>([]);
-  const [imageUri, setImageUri] = useState<string>();
   const [previewImages, setPreviewImages] = useState<string[]>([]);
   const navigation =
     useNavigation<
       NativeStackNavigationProp<AuthRoutesParamList, "Cadastro de Animal">
     >();
-  
-    useEffect(() => {
-      (async () => {
-        if (Platform.OS !== 'web') {
-          const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-          if (status !== 'granted') {
-            alert('Sorry, we need camera roll permissions to make this work!');
-          }
-        }
-      })();
-    }, []);
 
-    const handleImagePicked = async (pickerResult:ImagePicker.ImagePickerResult) => {
-      try {
-        if (pickerResult.cancelled) {
-          alert('Upload cancelled');
-          return;
-        } else {
-         const img = await fetchImageFromUri(pickerResult.uri);
-         console.log(JSON.stringify(img));
-         // var file = new File([img], "name");
-         const file = {
-          uri: pickerResult.uri,
-          type: mime.getType(pickerResult.uri),
-          name: pickerResult.uri.split('/').pop() // pega o ultimo item cortado por '/'
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
         }
-          setImages(old => [...old, file]);
-          console.log(file, "type pícker");
-          setImageUri(pickerResult.uri);
-          //const uploadUrl = await uploadImage('demo.jpg', img);
-          //downloadImage(uploadUrl);
-        }
-      } catch (e) {
-        console.log(e);
-        alert('Upload failed');
       }
-    };
+    })();
+  }, []);
 
-    const fetchImageFromUri = async (uri: string) => {
-      const response = await fetch(uri);
-      const blob = await response.blob();
-      return blob;
-    };  
-
-    const pickImage = async () => {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.Images,
-        allowsEditing: true,
-        aspect: [1, 1],
-        quality: 1,
-      }); 
-      handleImagePicked(result) 
+  const handleImagePicked = async (pickerResult: ImagePicker.ImagePickerResult) => {
+    try {
+      if (pickerResult.cancelled) {
+        alert('Upload cancelled');
+        return;
+      } else {
+        setPreviewImages([...previewImages, pickerResult.uri as any]);
+        setImages([...images, pickerResult as any]);
+      }
+    } catch (e) {
+      console.log(e);
+      alert('Upload failed');
     }
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+    handleImagePicked(result)
+  }
 
 
   async function eventoCriarAnimal() {
@@ -167,31 +149,46 @@ function Animal() {
         selectTemp,
         selectSoci,
         selectVive
-      ); 
+      );
       await criarImgAnimal(
-          images,
-          String(id_animal) 
-      )
-      ;
+        images,
+        String(id_animal)
+      );
 
       alert("Animal cadastrado com sucesso e aguardando análise.");
-      navigation.navigate("Home");
+      // navigation.navigate("Home");
     } catch (error) {
       console.log(error);
       alert("Erro ao criar animal.");
     }
   }
 
+  function removerImagem(index: number) {
+    const imagesTemp = [...images.slice(0, index), ...images.slice(index + 1, images.length)]
+    const imagesPreviewTemp = [...previewImages.slice(0, index), ...previewImages.slice(index + 1, previewImages.length)]
+    console.log('312312', imagesPreviewTemp);
+    setImages([...imagesTemp]);
+    setPreviewImages([...imagesPreviewTemp]);
+
+  }
+
   return (
-    <ScrollView style={{backgroundColor: 'white'}}>
-      <View 
-      style={{
+    <ScrollView style={{ backgroundColor: 'white' }}>
+      <View
+        style={{
           alignItems: "center",
           justifyContent: "center",
           marginTop: "15%",
           backgroundColor: "#f8f8f8",
         }}>
-        {imageUri && <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />}
+        {
+          previewImages.map((imageUri, index) => <View key={imageUri + index}>
+            <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+            <Pressable onPress={() => removerImagem(index)} style={styles.botao}>
+              <Text>Remover</Text>
+            </Pressable>
+          </View>)
+        }
         <Button title="Pick an image from camera roll" onPress={pickImage} />
       </View>
       <View
@@ -201,7 +198,7 @@ function Animal() {
           marginTop: "15%",
           marginBottom: "10%",
           backgroundColor: 'white',
-          
+
         }}
       >
         <Text
@@ -306,7 +303,7 @@ function Animal() {
         <Pressable onPress={eventoCriarAnimal} style={styles.botao}>
           <Text>Cadastrar</Text>
         </Pressable>
-        
+
       </View>
     </ScrollView>
   );
@@ -321,7 +318,7 @@ const styles = StyleSheet.create({
     color: "black",
     borderRadius: 2,
     backgroundColor: "#f8f8f8",
-    
+
   },
 
   button: {
