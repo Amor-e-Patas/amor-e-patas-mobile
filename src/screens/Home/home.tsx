f2f0b285733393b381daad482cd7a7326f9581d0
Uelyda Luiza <uelyda-luiza@hotmail.com>
2021-11-04 21:12:46 -0300

    Alterando visualização de imagens

--- src/screens/Home/home.tsx
+++ src/screens/Home/home.tsx
@@ -109,7 +109,7 @@ export default function Home() {
                 <Image
                   style={styles.imgani}
                   source={{
-                    uri: `http://192.168.1.64:3333/${animal?.images[0].filepath}`,
+                    uri: `http://192.168.1.69:3333/${animal?.images[0].filepath}`,
                   }}
                 ></Image>
                 <Text
