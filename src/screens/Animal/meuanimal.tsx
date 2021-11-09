abc3be0944ca49250056bd9dfffafb45ff9a5e16
ana.santana <anaflavia2309@gmail.com>
2021-11-08 23:33:38 -0300

    Ajustando layout da página meu animal

--- src/screens/Animal/meuanimal.tsx
+++ src/screens/Animal/meuanimal.tsx
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Image, StyleSheet, ScrollView, Modal, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  Modal,
+  Pressable,
+} from "react-native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { AuthRoutesParamList } from "../../routes/AuthRoutes.routes";
 import { getAnimal, Animal, deleteAnimal } from "../../service/animal";
@@ -7,10 +15,10 @@ import { useNavigation, useRoute } from "@react-navigation/native";
 import Hr from "../../components/Hr";
 import { TextInput } from "react-native-gesture-handler";
 import { BackgroundImage } from "react-native-elements/dist/config";
-import { RectButton } from 'react-native-gesture-handler';
+import { RectButton } from "react-native-gesture-handler";
 
 interface NoticiaParams {
-  animalId: number
+  animalId: number;
 }
 
 export default function Home() {
@@ -40,26 +48,27 @@ export default function Home() {
   const abrirModalImagem = (imageUrl: string) => {
     setModalImageUrl(imageUrl);
     setShowImageModal(!showImageModal);
-  }
+  };
 
   const excluirAnimal = async () => {
     await deleteAnimal(excluirAnimalId);
     console.log(excluirAnimalId);
     setShowExcluirModal(false);
     navigation.navigate("Meus animais");
-  }
+  };
 
   const abrirAlterarAnimal = (animalId: number) => {
     navigation.navigate("Alterar animal", {
       animalId,
     });
-  }
+  };
 
   return (
     <ScrollView
       style={{
         backgroundColor: "white",
-      }}>
+      }}
+    >
       <Modal
         animationType="slide"
         transparent={true}
@@ -71,14 +80,13 @@ export default function Home() {
         <View style={styles.centeredView}>
           <Pressable
             onPress={() => setShowImageModal(!showImageModal)}
-          >
-            <Text>X</Text>
-          </Pressable>
+          ></Pressable>
           <Image
-            style={styles.stretch}
+            style={styles.stretch2}
             source={{
               uri: modalImagelUrl,
-            }}></Image>
+            }}
+          ></Image>
         </View>
       </Modal>
       <Modal
@@ -87,43 +95,89 @@ export default function Home() {
         visible={showExcluirModal}
       >
         <View style={styles.centeredView}>
-          <Pressable onPress={excluirAnimal}><Text>Sim</Text></Pressable>
-          <Pressable onPress={() => { setShowExcluirModal(false) }}><Text>Não</Text></Pressable>
+        <Text style={styles.rText2}>Deseja realmente excluir?</Text>
+          <Pressable onPress={excluirAnimal}>
+            <Text style={styles.rText}>Sim</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => {
+              setShowExcluirModal(false);
+            }}
+          >
+            <Text style={styles.rText}>Não</Text>
+          </Pressable>
         </View>
       </Modal>
       <View
         style={{
           alignItems: "center",
           backgroundColor: "white",
-          marginBottom: "100%"
+          marginBottom: "100%",
         }}
       >
         <View>
           <Image
             style={styles.stretch}
             source={{
-              uri: `http://192.168.1.69:3333/${animal?.images[0].filepath}`,
-            }}></Image>
-          <Text>{animal?.nome_ani}</Text>
-          {
-            animal?.images.map(image => <Pressable onPress={() => abrirModalImagem(`http://192.168.1.69:3333/${image.filepath}`)}>
+              uri: `http://192.168.1.64:3333/${animal?.images[0].filepath}`,
+            }}
+          ></Image>
+
+          {animal?.images.map((image) => (
+            <Pressable
+              onPress={() =>
+                abrirModalImagem(`http://192.168.1.64:3333/${image.filepath}`)
+              }
+            >
               <Image
                 style={styles.previewImage}
                 source={{
-                  uri: `http://192.168.1.69:3333/${image.filepath}`,
-                }}></Image>
-            </Pressable>)
-          }
+                  uri: `http://192.168.1.64:3333/${image.filepath}`,
+                }}
+              ></Image>
+            </Pressable>
+          ))}
+          <View></View>
         </View>
-        <RectButton onPress={() => abrirAlterarAnimal(animal?.id_animal as number)}>
-          <Text>Editar</Text>
-        </RectButton>
-        <RectButton onPress={() => {
-          setExcluirIdAnimal(animal?.id_animal as number);
-          setShowExcluirModal(true);
-        }}>
-          <Text>Excluir</Text>
-        </RectButton>
+
+        <View>
+          <Text
+            style={{
+              color: "purple",
+              fontFamily: "Raleway_600SemiBold",
+              fontSize: 20,
+              textAlign: "center",
+            }}
+          >
+            {animal?.nome_ani}
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            alignItems: "center",
+            width: 400,
+            marginTop: 10,
+          }}
+        >
+          <RectButton
+            onPress={() => abrirAlterarAnimal(animal?.id_animal as number)}
+          >
+            <Text style={styles.rText}>Editar</Text>
+          </RectButton>
+
+          <RectButton
+            onPress={() => {
+              setExcluirIdAnimal(animal?.id_animal as number);
+              setShowExcluirModal(true);
+            }}
+          >
+            <Text style={styles.rText}>Excluir</Text>
+          </RectButton>
+        </View>
       </View>
     </ScrollView>
   );
@@ -134,16 +188,42 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: 22
+    backgroundColor: "white",
+    flexDirection: "row",
+    
   },
+
   stretch: {
-    width: 200,
-    height: 200,
+    width: 380,
+    height: 350,
     resizeMode: "stretch",
   },
+
+  stretch2: {
+    width: 380,
+    height: 350,
+    resizeMode: "stretch",
+    
+  },
+
+  rText: {
+    fontFamily: "Raleway_600SemiBold",
+    fontSize: 17,
+    margin: "2%",
+    textAlign: "center",
+  },
+
+  rText2: {
+    fontFamily: "Raleway_600SemiBold",
+    fontSize: 17,
+    margin: "2%",
+    textAlign: "center",
+  },
+
   previewImage: {
-    width: 50,
-    height: 50,
+    width: 70,
+    height: 70,
+    marginTop: 5,
   },
   titulo: {
     fontFamily: "Raleway_600SemiBold",
