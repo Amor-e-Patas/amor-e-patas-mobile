78e50f5b221d731bf7572b9716de8f8dee057125
ana.santana <anaflavia2309@gmail.com>
2021-11-10 22:21:03 -0300

    Adicionando o menu de animais e de perfil

--- src/routes/AuthRoutes.routes.tsx
+++ src/routes/AuthRoutes.routes.tsx
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import Login from '../screens/Login';
-import SignUp from '../screens/SignUp';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Login from "../screens/Login";
+import SignUp from "../screens/SignUp";
 import Home from "../screens/Home/home";
 import LogOut from "../screens/Login/logout";
 import AlterarUser from "../screens/User/alterarUser";
@@ -10,14 +10,14 @@ import AlterarLogin from "../screens/User/alterarLogin";
 import AlterarEndereco from "../screens/User/alterarEndereco";
 import AlterarTelefone from "../screens/User/alterarTelefone";
 import { AuthContext } from "../contexts/auth";
-import MeusAnimais from '../screens/Animal/meusanimais';
+import MeusAnimais from "../screens/Animal/meusanimais";
 import CriarAnimal from "../screens/Animal/criaranimal";
 import MeuAnimal from "../screens/Animal/meuanimal";
 import Animal from "../screens/Animal/animal";
 import AlterarAnimal from "../screens/Animal/alteraranimal";
 import Post from "../screens/Noticia/criarnoticia";
 import AnaliseAnimalDesaparecidos from "../screens/Animal/analisedesaparecido";
-import Noticias from '../screens/Noticia/noticias';
+import Noticias from "../screens/Noticia/noticias";
 import Noticia from "../screens/Noticia/noticia";
 import AlterarNoticia from "../screens/Noticia/alterarnoticia";
 import NoticiasAdm from "../screens/Noticia/noticiasadm";
@@ -26,77 +26,93 @@ import Desaparecidos from "../screens/Animal/desaparecidos";
 import Analise from "../screens/Animal/analise";
 import MeuPerfil from "../screens/User/meuperfil";
 import MeusAnimaisDesaparecidos from "../screens/Animal/meusanimaisdesaparecidos";
-
+import MenuAnimais from "../screens/Animal/menu";
 
 export type AuthRoutesParamList = {
-  'Cadastre-se': undefined
-  'Login': undefined
-  'Home': undefined
-  'Cadastro de Animal': undefined
-  'Log out': undefined
-  'Alterar usuário': undefined
-  'Alterar login': undefined
-  'Alterar endereço': undefined
-  'Alterar telefone': undefined
-  'Cadastrar Notícia': undefined
-  'Meus animais': undefined
-  'Meus animais desaparecidos': undefined
-  "Analise de animal desaparecido": undefined
-  'Animal': {
-    animalId: number
-  },
-  'Meu animal': {
-    animalId: number
-  },
-  'Alterar animal': {
-    animalId: number
-  },
-  'Blog': undefined
-  'Noticia': {
-    noticiaId: number
-  },
-  'Noticias cadastradas': undefined,
-  'Alterar noticia': {
-    noticiaId: number
-  },
-  'Adotar': undefined,
-  'Desaparecidos': undefined,
-  'Analise de animal': undefined,
-  'Navigator': undefined,
-  'Meu perfil': undefined,
-
-}
-
+  "Cadastre-se": undefined;
+  Login: undefined;
+  Home: undefined;
+  "Cadastro de Animal": undefined;
+  "Log out": undefined;
+  "Alterar usuário": undefined;
+  "Alterar login": undefined;
+  "Alterar endereço": undefined;
+  "Alterar telefone": undefined;
+  "Cadastrar Notícia": undefined;
+  "Meus animais": undefined;
+  "Meus animais desaparecidos": undefined;
+  "Analise de animal desaparecido": undefined;
+  Animal: {
+    animalId: number;
+  };
+  "Meu animal": {
+    animalId: number;
+  };
+  "Alterar animal": {
+    animalId: number;
+  };
+  Blog: undefined;
+  Noticia: {
+    noticiaId: number;
+  };
+  "Noticias cadastradas": undefined;
+  "Alterar noticia": {
+    noticiaId: number;
+  };
+  Adotar: undefined;
+  Desaparecidos: undefined;
+  "Analise de animal": undefined;
+  Navigator: undefined;
+  "Meu perfil": undefined;
+  "Menu animais": undefined;
+};
 
 const Drawer = createDrawerNavigator<AuthRoutesParamList>();
 
 const AuthStack = createNativeStackNavigator<AuthRoutesParamList>();
-
 
 function AuthRoutes() {
   const { isAuthenticated, isAdm } = useContext(AuthContext);
   console.log(isAuthenticated, isAdm, "autenticação");
   return (
     <AuthStack.Navigator>
-      <AuthStack.Screen name="Navigator" options={{ headerShown: false }} component={MyDrawer} />
-      {
-        isAuthenticated ? <>
+      <AuthStack.Screen
+        name="Navigator"
+        options={{ headerShown: false }}
+        component={MyDrawer}
+      />
+      {isAuthenticated ? (
+        <>
           <AuthStack.Screen name="Animal" component={Animal} />
           <AuthStack.Screen name="Meu animal" component={MeuAnimal} />
           <AuthStack.Screen name="Alterar animal" component={AlterarAnimal} />
           <AuthStack.Screen name="Noticia" component={Noticia} />
-
+          <AuthStack.Screen name="Meus animais" component={MeusAnimais} />
+          <AuthStack.Screen name="Meus animais desaparecidos" component={MeusAnimaisDesaparecidos}/>
           <AuthStack.Screen name="Alterar usuário" component={AlterarUser} />
           <AuthStack.Screen name="Alterar login" component={AlterarLogin} />
-          <AuthStack.Screen name="Alterar endereço" component={AlterarEndereco} />
-          <AuthStack.Screen name="Alterar telefone" component={AlterarTelefone} />
-          {
-            isAdm ? <>
-              <AuthStack.Screen name="Alterar noticia" component={AlterarNoticia} />
-            </> : <></>
-          }
-        </> : <></>
-      }
+          <AuthStack.Screen
+            name="Alterar endereço"
+            component={AlterarEndereco}
+          />
+          <AuthStack.Screen
+            name="Alterar telefone"
+            component={AlterarTelefone}
+          />
+          {isAdm ? (
+            <>
+              <AuthStack.Screen
+                name="Alterar noticia"
+                component={AlterarNoticia}
+              />
+            </>
+          ) : (
+            <></>
+          )}
+        </>
+      ) : (
+        <></>
+      )}
     </AuthStack.Navigator>
   );
 }
@@ -104,35 +120,42 @@ function AuthRoutes() {
 export function MyDrawer() {
   const { isAuthenticated, isAdm } = useContext(AuthContext);
   return (
-
     <Drawer.Navigator>
-
       <Drawer.Screen name="Home" component={Home} />
 
-      {!isAuthenticated ? <>
-        <Drawer.Screen name="Login" component={Login} />
-        <Drawer.Screen name="Cadastre-se" component={SignUp} />
-      </> : <>
-        <Drawer.Screen name="Desaparecidos" component={Desaparecidos} />
-        <Drawer.Screen name="Adotar" component={Adotar} />
-        <Drawer.Screen name="Blog" component={Noticias} />
-        <Drawer.Screen name="Meu perfil" component={MeuPerfil} />
-        <Drawer.Screen name="Meus animais" component={MeusAnimais} />
-        <Drawer.Screen name="Meus animais desaparecidos" component={MeusAnimaisDesaparecidos} />
-        <Drawer.Screen name="Cadastro de Animal" component={CriarAnimal} />
-        {
-          isAdm ? <>
-            <Drawer.Screen name="Noticias cadastradas" component={NoticiasAdm} />
-            <Drawer.Screen name="Cadastrar Notícia" component={Post} />
-            <Drawer.Screen name="Analise de animal" component={Analise} />
-            <Drawer.Screen name="Analise de animal desaparecido" component={AnaliseAnimalDesaparecidos} />
-          </> : <></>
-        }
-        <Drawer.Screen name="Log out" component={LogOut} />
-      </>}
-
+      {!isAuthenticated ? (
+        <>
+          <Drawer.Screen name="Login" component={Login} />
+          <Drawer.Screen name="Cadastre-se" component={SignUp} />
+        </>
+      ) : (
+        <>
+          <Drawer.Screen name="Desaparecidos" component={Desaparecidos} />
+          <Drawer.Screen name="Adotar" component={Adotar} />
+          <Drawer.Screen name="Blog" component={Noticias} />
+          <Drawer.Screen name="Meu perfil" component={MeuPerfil} />
+          <Drawer.Screen name="Menu animais" component={MenuAnimais} />
+          <Drawer.Screen name="Cadastro de Animal" component={CriarAnimal} />
+          {isAdm ? (
+            <>
+              <Drawer.Screen
+                name="Noticias cadastradas"
+                component={NoticiasAdm}
+              />
+              <Drawer.Screen name="Cadastrar Notícia" component={Post} />
+              <Drawer.Screen name="Analise de animal" component={Analise} />
+              <Drawer.Screen
+                name="Analise de animal desaparecido"
+                component={AnaliseAnimalDesaparecidos}
+              />
+            </>
+          ) : (
+            <></>
+          )}
+          <Drawer.Screen name="Log out" component={LogOut} />
+        </>
+      )}
     </Drawer.Navigator>
-
   );
 }
 
---
+++ src/screens/Animal/menu.tsx
@@ -0,0 +1,134 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  StyleSheet,
+  Pressable,
+  Image,
+} from "react-native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useNavigation } from "@react-navigation/core";
+import { AuthRoutesParamList } from "../../routes/AuthRoutes.routes";
+import { RectButton } from "react-native-gesture-handler";
+
+export default function MenuAnimais() {
+  const navigation =
+    useNavigation<
+      NativeStackNavigationProp<AuthRoutesParamList, "Alterar endereço">
+    >();
+
+  return (
+    <ScrollView
+    style={{
+      backgroundColor: "white",
+    }}>
+      <View
+        style={{
+          alignItems: "center",
+          marginBottom: "auto",
+        }}
+      >
+        <View
+          style={{
+            flex: 2,
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            alignItems: "center",
+            width: 400,
+            marginTop: 100,
+          }}
+        >
+          <View style={styles.cards}>
+            <Image
+              style={styles.stretch}
+              source={require("../../../assets/addanimal.png")}
+            ></Image>
+            <RectButton onPress={() => navigation.navigate("Cadastro de Animal")}>
+              <Text style={styles.text}>Cadastrar animal</Text>
+            </RectButton>
+          </View>
+
+          <View style={styles.cards}>
+            <Image
+              style={styles.stretch}
+              source={require("../../../assets/analise.png")}
+            ></Image>
+            <RectButton onPress={() => navigation.navigate("Alterar endereço")}>
+              <Text style={styles.text}>Animais em análise</Text>
+            </RectButton>
+          </View>
+
+          <View style={styles.cards}>
+            <Image
+              style={styles.stretch}
+              source={require("../../../assets/animais.png")}
+            ></Image>
+            <RectButton onPress={() => navigation.navigate("Meus animais")}>
+              <Text style={styles.text}>Meus animais</Text>
+            </RectButton>
+          </View>
+
+          <View style={styles.cards}>
+            <Image
+              style={styles.stretch}
+              source={require("../../../assets/desaparecidos.png")}
+            ></Image>
+            <RectButton onPress={() => navigation.navigate("Meus animais desaparecidos")}>
+              <Text style={styles.text}>Meus animais desaparecidos</Text>
+            </RectButton>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+const styles = StyleSheet.create({
+  input: {
+    alignSelf: "stretch",
+    backgroundColor: "#f8f8f8",
+    borderBottomColor: "#f8f8f8",
+    borderBottomWidth: 1,
+    height: 40,
+    borderRadius: 6,
+    padding: 10,
+    width: "90%",
+    marginLeft: "5%",
+    marginTop: "2%",
+  },
+  botao: {
+    height: 40,
+    width: 150,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "#B7E394",
+    alignItems: "center",
+    borderRadius: 4,
+  },
+  cards: {
+    height: 250,
+    width: "45%",
+    marginBottom: 5,
+    marginLeft: 5,
+    marginTop: 5,
+    alignContent: "center",
+    borderWidth: 0.3,
+    borderRadius: 9,
+  },
+  stretch: {
+    width: 120,
+    height: 120,
+    resizeMode: "stretch",
+    marginHorizontal:35,
+    marginVertical:35
+  },
+  
+  text: {
+    color: "purple",
+    fontFamily: "Raleway_600SemiBold",
+    textAlign: "center",
+  },
+});
