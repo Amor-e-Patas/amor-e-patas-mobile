25ecf974cd6b6fba43fa7b1be5b2fbb0ad1f7abc
ana.santana <anaflavia2309@gmail.com>
2021-11-10 22:28:21 -0300

    Ajustando o Menu de perfil

--- src/screens/User/meuperfil.tsx
+++ src/screens/User/meuperfil.tsx
@@ -5,7 +5,8 @@ import {
   TextInput,
   ScrollView,
   StyleSheet,
-  Pressable
+  Pressable,
+  Image,
 } from "react-native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/core";
@@ -14,24 +15,74 @@ import { RectButton } from "react-native-gesture-handler";
 
 export default function MeuPerfil() {
   const navigation =
-    useNavigation<NativeStackNavigationProp<AuthRoutesParamList, "Alterar endereço">>();
+    useNavigation<
+      NativeStackNavigationProp<AuthRoutesParamList, "Alterar endereço">
+    >();
 
   return (
-    <ScrollView>
+    <ScrollView
+    style={{
+      backgroundColor: "white",
+    }}>
       <View
         style={{
           alignItems: "center",
-          justifyContent: "center",
-          marginTop: "5%"
-
+          marginBottom: "auto",
         }}
       >
-        <RectButton onPress={() => navigation.navigate("Alterar usuário")}><Text>Atualizar meus dados pessoais</Text></RectButton>
-      <RectButton onPress={() => navigation.navigate("Alterar endereço")}><Text>Atualizar meu endereço</Text></RectButton>
-      <RectButton onPress={() => navigation.navigate("Alterar telefone")}><Text>Atualizar meu contato</Text></RectButton>
-      <RectButton onPress={() => navigation.navigate("Alterar login")}><Text>Atualizar senha</Text></RectButton>
-    </View>
-    </ScrollView >
+        <View
+          style={{
+            flex: 2,
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            alignItems: "center",
+            width: 400,
+            marginTop: 100,
+          }}
+        >
+          <View style={styles.cards}>
+            <Image
+              style={styles.stretch}
+              source={require("../../../assets/perfil.png")}
+            ></Image>
+            <RectButton onPress={() => navigation.navigate("Alterar usuário")}>
+              <Text style={styles.text}>Atualizar meus dados pessoais</Text>
+            </RectButton>
+          </View>
+
+          <View style={styles.cards}>
+            <Image
+              style={styles.stretchend}
+              source={require("../../../assets/endereco2.png")}
+            ></Image>
+            <RectButton onPress={() => navigation.navigate("Alterar endereço")}>
+              <Text style={styles.text}>Atualizar meu endereço</Text>
+            </RectButton>
+          </View>
+
+          <View style={styles.cards}>
+            <Image
+              style={styles.stretchcall}
+              source={require("../../../assets/celular.png")}
+            ></Image>
+            <RectButton onPress={() => navigation.navigate("Alterar telefone")}>
+              <Text style={styles.text}>Atualizar meu contato</Text>
+            </RectButton>
+          </View>
+
+          <View style={styles.cards}>
+            <Image
+              style={styles.stretchpass}
+              source={require("../../../assets/pass.png")}
+            ></Image>
+            <RectButton onPress={() => navigation.navigate("Alterar login")}>
+              <Text style={styles.text}>Atualizar senha</Text>
+            </RectButton>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 const styles = StyleSheet.create({
@@ -56,5 +107,48 @@ const styles = StyleSheet.create({
     backgroundColor: "#B7E394",
     alignItems: "center",
     borderRadius: 4,
+  },
+  cards: {
+    height: 250,
+    width: "45%",
+    marginBottom: 5,
+    marginLeft: 5,
+    marginTop: 5,
+    alignContent: "center",
+    borderWidth: 0.3,
+    borderRadius: 9,
+  },
+  stretch: {
+    width: 150,
+    height: 150,
+    resizeMode: "stretch",
+    marginHorizontal:15,
+    marginVertical:20
+  },
+  stretchend: {
+    width: 120,
+    height: 120,
+    resizeMode: "stretch",
+    marginHorizontal:35,
+    marginVertical:35
+  },
+  stretchcall: {
+    width: 110,
+    height: 110,
+    resizeMode: "stretch",
+    marginHorizontal:45,
+    marginVertical:40
+  },
+  stretchpass: {
+    width: 120,
+    height: 120,
+    resizeMode: "stretch",
+    marginHorizontal:35,
+    marginVertical:35
+  },
+  text: {
+    color: "purple",
+    fontFamily: "Raleway_600SemiBold",
+    textAlign: "center",
   },
 });
